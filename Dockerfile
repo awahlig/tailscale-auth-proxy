ba8d8aecd51730abed64c9a7775ef14b6ce86511
Arkadiusz Wahlig <arkadiusz@wahlig.eu>
2023-11-05 12:15:18 +0100

    Working version

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM nginx
+
+ENV UPSTREAM http://localhost
+ENV USERS {}
+ENV REAL_CLIENT_IP_HEADER X-Real-IP
+ENV TRUSTED_IP 127.0.0.1
+
+COPY nginx.conf /etc/nginx/
+COPY default.conf.template /etc/nginx/templates/
+COPY tailscale.js /etc/nginx
---
+++ tailscale.js
@@ -0,0 +1,54 @@
+const querystring = require("querystring");
+
+const TAILSCALE_SOCK = "/var/run/tailscale/tailscaled.sock";
+const TAILSCALE_HOST = "local-tailscaled.sock";
+
+const USERS = JSON.parse(process.env["USERS"]);
+
+async function auth(r) {
+  try {
+    const userpass = await getUserPass(r.remoteAddress);
+    const auth = Buffer.from(userpass).toString("base64");
+    r.headersOut["Authorization"] = `Basic ${auth}`;
+    r.return(200);
+  } catch (e) {
+    r.error(e);
+    r.return(401);
+  }
+}
+
+async function getUserPass(addr) {
+  const info = await whois(addr);
+  const user = info.UserProfile.LoginName;
+  const userpass = USERS[user] ?? USERS["default"];
+  if (!userpass) {
+    throw `unknown user: ${user}`;
+  }
+  return userpass;
+}
+
+async function whois(addr) {
+  const addrq = querystring.escape(addr);
+  const url = `http://unix://${TAILSCALE_SOCK}:/localapi/v0/whois?addr=${addrq}`;
+  const resp = await ngx.fetch(url, {
+    headers: {
+      "Host": TAILSCALE_HOST,
+    }
+  });
+  if (resp.status !== 200) {
+    const text = await resp.text();
+    throw `whois failed: ${text}`;
+  }
+  return await resp.json();
+}
+
+function test(r) {
+  r.error(JSON.stringify(r.headersIn));
+  r.error(r.remoteAddress);
+  return "";
+}
+
+export default {
+  auth,
+  test,
+};
